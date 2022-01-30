52828cbe8e4081e0ca2dd8bfe13e891b01a1f2a0
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2022-01-30 20:05:07 +0200

    change avatars

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,16 +1,18 @@
 const {Conflict} = require("http-errors")
-const gravatar = require("gravatar")
+const {AvatarGenerator} = require("random-avatar-generator")
+// const gravatar = require("gravatar")
 const {User} = require("../../model/index")
 
 const register = async (req, res) => {
-  console.log("req", req)
-  const {email, password} = req.body
-  const avatarUrl = gravatar.url(email, {s: "250", r: "x", d: "robohash"})
+  const {email, password, name} = req.body
+  const generator = new AvatarGenerator()
+  const avatarUrl = generator.generateRandomAvatar()
+  // gravatar.url(email, { s: "250", r: "x", d: "wavatar" })
   const result = await User.findOne({email})
   if (result) {
     throw new Conflict(`Эта почта ${email} уже используется`)
   }
-  const newUser = new User({email, avatarUrl})
+  const newUser = new User({email, avatarUrl, name})
   newUser.setPassword(password)
   await newUser.save()
   res.status(201).json({
